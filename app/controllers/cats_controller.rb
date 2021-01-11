4706c58e9d18bc30f5ef4592d0f526b283597b93
“filippo20u” <Filippo.20u@gmail.com>
2021-01-11 15:11:33 -0800

    validations completed

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,51 @@
 class CatsController < ApplicationController
+
+
+    def index
+        cats = Cat.all
+        render json: cats
+        end
+  
+    # def create
+    # end
+
+
+    # def create
+    #     # Create a new cat
+    #     cat = Cat.create(cat_params)
+    
+    #     # respond with our new cat
+    #     render json: cat
+    #   end
+
+
+      def create
+        cat = Cat.create(cat_params)
+        if cat.valid?
+          render json: cat
+         else
+           render json: cat.errors, status: :unprocessable_entity
+         end
+      end
+    
+     
+
+  
+    # def update
+    # end
+  
+    # def destroy
+    # end
+  
+  
+
+
+ # Handle strong parameters, so we are secure
+ private
+ def cat_params
+   params.require(:cat).permit(:name, :age, :enjoys)
+ end
+
+
 end
+
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
+# # RSpec.describe Cat, type: :model do
+# #   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should validate name" do
+    cat = Cat.create
+    expect(cat.errors[:name]).to_not be_empty
+    expect(cat.errors[:age]).to_not be_empty
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate name" do
+    cat = Cat.create(name: 'Felix', age: 2, enjoys: 'Walks')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate name" do
+    cat = Cat.create(name: 'Felixtwwiwiwiwiwiwiwiwiw', age: 2, enjoys: 'Walks')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it "should validate name" do
+    cat = Cat.create(name: 'Felix', age: 2, enjoys: 'Wijiojiojiooijoijoijiojiojiojojoijiojoijiojiojojalks')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate name" do
+    cat = Cat.create(name: 'Felix', age: 1000, enjoys: 'Walks')
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+
 end
